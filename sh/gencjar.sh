778d1f2f77c4523df69f7c29aef4a8afdb55c8ce
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-09-01 22:43:34 +0100

    Generate OneJAR shell script works. :)

--- sh/gencjar.sh
+++ sh/gencjar.sh
@@ -3,8 +3,9 @@
 # using One-JAR bootstrap jar
 # http://one-jar.sourceforge.net
 
+appJar="Gaudi.jar"
 oneJar="one-jar-boot-0.97.jar"
-mainClass="One-Jar-Main-Class: org.stpettersen.gaudi.GaudiApp\r\n"
+mainClass="One-Jar-Main-Class: org.stpettersens.gaudi.GaudiApp\r\n"
 
 echo "**************************"
 echo "Generating all-in-one JAR."
@@ -14,8 +15,19 @@ mkdir cjar
 cp -r lib cjar
 cd cjar
 7z x lib/$oneJar 
-rm -f $oneJar
+rm -f lib/$oneJar
+rm -f lib/.dummy
 rm OneJar.class
 rm -r -f src
 echo $mainClass >> boot-manifest.mf
+mkdir main
+cd ..
+cp bin/$appJar cjar/main
+
+echo "Packaging..."
+cd cjar
+jar cfvm $appJar boot-manifest.mf .
+cd ..
+mv cjar/$appJar dist
+rm -f -r cjar
 echo "Done."
