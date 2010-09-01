3ad0196cc0a7e352e434fd9696492ec0a07c5f0f
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-09-01 20:12:05 +0100

    Added detection for platform to use ';' or ':' in -cp.

--- sh/build.sh
+++ sh/build.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 # Build Gaudi
+
+sep=";" # initially use Windows, ";"
+
+# For Linux, Unix/xBSD - use ":"
+if [[ `uname` =~ .*n.*x|.+BSD ]]; then
+	sep=":"
+fi
 
 pkg="org/stpettersens/gaudi"
 jarF="Gaudi.jar"
-deps="lib/json_simple-1.1.jar;lib/commons-io-1.4.jar"
+deps="lib/json_simple-1.1.jar"$sep"lib/commons-io-1.4.jar"
 
 echo "Compiling..."
 scalac -classpath $deps -verbose -deprecation -unchecked -d bin src/$pkg/*.scala
