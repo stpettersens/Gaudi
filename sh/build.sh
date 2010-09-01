fbf482b397e099cf5923f61bf27d160615d31f4c
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-09-01 18:15:13 +0100

    Edited build shell script.

--- sh/build.sh
+++ sh/build.sh
@@ -3,9 +3,10 @@
 
 pkg="org/stpettersens/gaudi"
 jarF="Gaudi.jar"
+deps="lib/json_simple-1.1.jar;lib/commons-io-1.4.jar"
 
 echo "Compiling..."
-scalac -classpath ./lib -verbose -deprecation -unchecked -d bin src/$pkg/*.scala
+scalac -classpath $deps -verbose -deprecation -unchecked -d bin src/$pkg/*.scala
 
 echo "Packaging..."
 cp Manifest.mf bin
