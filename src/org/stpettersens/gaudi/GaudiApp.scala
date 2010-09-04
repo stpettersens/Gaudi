a4358edbe73d4496ebe52d7ae4e2043232dd7a0d
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-09-04 02:28:13 +0100

    Minor correction.

--- src/org/stpettersens/gaudi/GaudiApp.scala
+++ src/org/stpettersens/gaudi/GaudiApp.scala
@@ -128,7 +128,7 @@ object GaudiApp {
 	  println("Copyright (c) 2010 Sam Saint-Pettersen")
 	  println("\nReleased under the Apache License v2.")
 	  println("\nUsage: gaudi [-i|-v|-g|-m][-q -f <build file>][\":<operation>\"]")
-	  println("\n-i: Di`splay usage information and quit.")
+	  println("\n-i: Display usage information and quit.")
 	  println("-v: Display version information and quit.")
 	  println("-g: Generate native Gaudi build file (build.json).")
 	  println("-m: Generate GNU Makefile from build.json.")
