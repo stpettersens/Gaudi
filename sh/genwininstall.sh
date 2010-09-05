75b3f9be59c25e5653d0cc941d2fe570be49d744
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-09-05 17:39:06 +0100

    Added flexibility to build NSIS installer on *nix.

--- sh/genwininstall.sh
+++ sh/genwininstall.sh
@@ -3,9 +3,16 @@
 # using MakeNSIS with NSIS script
 # http://nsis.sourceforge.net
 
+makensis=makensis # For Windows, use as-is
+
+# For Unix-likes (Linux/Unix/xBSD/Darwin/Mac OS X) - use wine
+if [[ `uname` =~ .+n.+x|.+BSD|Darwin ]]; then
+     makensis="wine $makensis"
+fi
+
 if [[ -e "dist/gaudi.exe" ]]; then
 	echo "Generating Windows installer.."
-	makensis dist/gaudi_setup.nsi
+	$makensis dist/gaudi_setup.nsi
 	echo "Done."
 else
 	echo "Important! 'Run sh/genexe.sh' first."
