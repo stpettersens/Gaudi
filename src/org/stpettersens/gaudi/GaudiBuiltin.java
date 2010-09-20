5894314832cba4a5787c6897f4b9fe652cd7b77d
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-09-20 02:36:13 +0100

    Added license headers.

--- src/org/stpettersens/gaudi/GaudiBuiltin.java
+++ src/org/stpettersens/gaudi/GaudiBuiltin.java
@@ -1,6 +1,25 @@
+/*
+Gaudi platform agnostic build tool
+Copyright 2010 Sam Saint-Pettersen.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+For dependencies, please see LICENSE file.
+*/
 package org.stpettersens.gaudi;
 
+// NOTE: Interface for interoperability between GaudiPluginLoader
+// written in Java and GaudiCommand written in Scala
 public interface GaudiBuiltin {
 	public void execute(String cmd);
 }
-
