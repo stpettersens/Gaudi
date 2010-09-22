5516166aa08edaf92fc07a83d9c6e2b61a1d89d0
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-09-22 16:28:59 +0100

    Build GaudiLogger and GaudiLogging first.

--- sh/build.sh
+++ sh/build.sh
@@ -15,10 +15,13 @@ scalaLib="lib/scala-library.jar"
 jsonLib="lib/json_simple-1.1.jar"
 ioLib="lib/commons-io-1.4.jar"
 deps="."$sep$jsonLib$sep$ioLib$sep$groovyLib
+scSwitches="-verbose -deprecation -unchecked -d bin"
 
 echo "Compiling Gaudi..."
-scalac -classpath $deps src/$pkg/*.scala src/$pkg/*.java -verbose -deprecation -unchecked -d bin
+scalac src/$pkg/GaudiLogger.scala src/$pkg/GaudiLogging.java $scSwitches
+scalac src/$pkg/*.scala src/$pkg/*.java $scSwitches
 javac -classpath $deps -verbose -deprecation -d bin src/$pkg/*.java
+scalac -classpath $deps src/$pkg/*.scala src/$pkg/*.java $scSwitches
 
 echo "Packaging Gaudi..."
 cp Manifest.mf bin
