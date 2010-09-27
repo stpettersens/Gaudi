1d4b4e6791ddf6442ea020165956b9470a5e8f1e
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-09-27 17:02:45 +0100

    ...

--- plugins/ExamplePlugin.groovy
+++ plugins/ExamplePlugin.groovy
@@ -3,15 +3,17 @@
 	@author Sam-Saint-Pettersen
 	@usage Plug-in for Gaudi demonstration
 */
-import org.stpettersens.gaudi.GaudiPluginBase
-public class ExamplePlugin extends GaudiPluginBase {
-
-	def pName = "Example plug-in"
-
-	public boolean initialize() { // TEMPORARY, USE BINDING FOR INITABLE VARIABLE
-		return true
-	}
+import org.stpettersens.gaudi.GaudiPlugin
+public class ExamplePlugin extends GaudiPlugin {
 	
+	ExamplePlugin() {
+		pName = "Example plug-in"
+		pAction = "Display message"
+		pVersion = "1.0"
+		pAuthor = "Sam Saint-Pettersen"
+		pUrl = "http://github.com/stpettersens/Gaudi"
+		pInitable = true
+	}
 	public void run() {
 		println "Hello from ${pName}"
 	}
