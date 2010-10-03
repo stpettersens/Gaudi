1496584ec0e5eeddfb862d991b7c28277adcd14c
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2010-10-03 14:19:58 +0100

    TODO: Fix compile step.

--- sh/build.sh
+++ sh/build.sh
@@ -14,14 +14,12 @@ groovyLib="lib/groovy-all-1.7.4.jar"
 scalaLib="lib/scala-library.jar"
 jsonLib="lib/json_simple-1.1.jar"
 ioLib="lib/commons-io-1.4.jar"
-deps="."$sep$jsonLib$sep$ioLib$sep$groovyLib
-scSwitches="-verbose -deprecation -unchecked -d bin"
+deps="."$sep$scalaLib$sep$jsonLib$sep$ioLib$sep$groovyLib
+switches="-verbose -deprecation -d bin"
 
 echo "Compiling Gaudi..."
-scalac src/$pkg/GaudiLogger.scala src/$pkg/IGaudiLogger.java $scSwitches
-scalac src/$pkg/*.scala src/$pkg/*.java $scSwitches
-javac -classpath $deps -verbose -deprecation -d bin src/$pkg/*.java
-scalac -classpath $deps src/$pkg/*.scala src/$pkg/*.java $scSwitches
+# CIRCULAR DEPS PROBLEM TO FIX
+exit #
 
 echo "Packaging Gaudi..."
 cp Manifest.mf bin
