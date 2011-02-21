bfed0f53d04f6caee28eaff8a76fd3c8453a5dac
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2011-02-21 20:05:23 +0000

    ...

--- src/org/stpettersens/gaudi/GaudiBase.scala
+++ src/org/stpettersens/gaudi/GaudiBase.scala
@@ -18,19 +18,19 @@ For dependencies, please see LICENSE file.
 */
 package org.stpettersens.gaudi
 import java.util.Date
-import java.text.{DateFormat,SimpleDateFormat)
+import java.text.{DateFormat,SimpleDateFormat}
 import java.io.{PrintWriter,FileOutputStream,IOException}
 
 object GaudiBase {
 	
 	protected def logDump(logging: Boolean, message: String): Unit = {
-		val timestamp: Date = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss")
+		val timestamp: DateFormat = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss")
 		if(logging) {
 			try {
 				// TODO
 			}
 			catch {
-				case ioe: IOException => println(ioe.Message);
+				case ioe: IOException => println(ioe.getMessage);
 			}
 		}
 	}
@@ -44,11 +44,11 @@ class GaudiBase {
 	val LogFile: String = "gaudi.log";
 	
 	protected def logDump(message: String): Unit = {
-		val timestamp: Date = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss")
-		if(isLogging()) {
-			writeTo(
+		val timestamp: DateFormat = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss")
+		//if(isLogging()) {
+			/*writeTo(
 				LogFile, String.format("[{0}]\n{1}", timestamp, message), true
-			)
-		}
+			)*/
+		//}
 	}
 }
