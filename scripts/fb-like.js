dcdb8398ca43dbd4f573a27fdfbce69f0b77e36c
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2011-09-25 15:22:32 +0200

    Edited scripts/fb-like.js via GitHub

--- scripts/fb-like.js
+++ scripts/fb-like.js
@@ -1,4 +1,7 @@
-function breakOutFrame {
+/*
+    Break out of the gaudi-build.tk frame.
+*/
+function breakoutFrame {
     if(top.location != location) {
         top.location.href = document.location.href;
     }
