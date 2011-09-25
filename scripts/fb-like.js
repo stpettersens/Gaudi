309ad8f6b516a9b6b651da8f18ec29bb28e0e60d
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2011-09-25 15:15:33 +0200

    TODO: fb-like.js -> break_frame.js.

--- scripts/fb-like.js
+++ scripts/fb-like.js
@@ -1,10 +1,6 @@
-/*
-    Code to inetgrate Facebook like functionality.
-*/
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+function breakOutFrame {
+    if(top.location != location) {
+        top.location.href = document.location.href;
+    }
+}
+breakoutFrame();
