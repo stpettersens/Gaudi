4328fb5e7b8f467ec471072e7710c53009918eb2
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2014-10-29 16:58:53 +0000

    Get txtrevise utility. Should work now with Travis CI!

--- contrib/getDependencies.py
+++ contrib/getDependencies.py
@@ -17,18 +17,18 @@
 
 import sys
 from subprocess import call
-import urllib
 
 def getDependencies(client):
 
-	deps = ["Scala 2.9.3", "JSON.simple 1.1", "Apache Commons IO 2.2"]
+	deps = ["Scala 2.9.3", "txtrevise utility", "JSON.simple 1.1", "Apache Commons IO 2.2"]
 
 	urls = [
 	"http://www.scala-lang.org/files/archive/scala-2.9.3.tgz", 
+	"https://raw.githubusercontent.com/stpettersens/txtrevise/master/python/txtrevise.py",
 	"http://json-simple.googlecode.com/files/json_simple-1.1.jar",
 	"http://mirror.gopotato.co.uk/apache//commons/io/binaries/commons-io-2.4-bin.zip"]
 
-	dests = ["scala-2.9.3.tgz", "json_simple-1.1.jar", "commons-2.4.bin.zip"]
+	dests = ["scala-2.9.3.tgz", "txtrevise.py", "json_simple-1.1.jar", "commons-2.4.bin.zip"]
 
 	if client == "travis":
 		pass
@@ -36,11 +36,14 @@ def getDependencies(client):
 		deps.pop(0)
 		urls.pop(0)
 		dests.pop(0)
+		deps.pop(1)
+		urls.pop(1)
+		dests.pop(1)
 
 	while len(deps) > 0:
 		current =  dests.pop(0)
 		print "Downloading {0}...".format(deps.pop(0))
-		urllib.urlretrieve(urls.pop(0), current)
+		call(["wget", "-O", current, urls.pop(0)])
 
 		# Extract TAR files.
 		if current.find(".tgz") != -1 or current.find("tar.gz") != -1:
@@ -54,6 +57,13 @@ def getDependencies(client):
 			call(["unzip", "-qq", current])
 			call(["cp", "commons-io-2.4/commons-io-2.4.jar", "lib/commons-io-2.2.jar"])
 			call(["rm", "-f", "-r", "commons-io-2.4", current])
+
+		# Copy Python files.
+		if current.find(".py") != -1:
+			call(["chmod", "+x", current])
+			call(["mv", current, current[:9]])
+			call(["sudo", "cp", current[:9], "/usr/bin"])
+			call(["rm", current[:9]])
 
 		# Copy JARs.
 		if current.find(".jar") != -1:
